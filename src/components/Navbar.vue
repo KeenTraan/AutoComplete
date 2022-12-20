5125f9143f9817a5f36156340eca8e4650a0aed2
KeenTraan <tranmanhphong3103@gmail.com>
2022-12-20 14:41:23 +0700

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="nav-bar">
+    <h2 style="color:aliceblue">Tranning Intern</h2>
+    <nav >
+      <router-link v-for="link in navlink" :key="link.id" :to="link.path">
+        <strong class="header"> {{ link.name }}</strong>
+      </router-link>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavbarComponent",
+  data() {
+    return {
+      navlink: [
+        {id: 1, name: "Home", path: "/" },
+        {id: 2, name: "Auto Complete", path: "/autocomplete" },
+      ],
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+.nav-bar{
+  background-color: black;
+  width: 200px;
+  height: 100vh;
+}
+nav {
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  margin-top: 20px;
+  a{
+    text-decoration: none;
+    color: white;
+    &.router-link-exact-active {
+        color: skyblue;;
+      }
+  }
+}
+
+
+</style>
---
+++ src/views/AutoComplete.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="auto-layout">
+    <Navbar />
+    <div class="input-layout">
+      <img src="@/assets/search.png" alt="" />
+      <input type="search" placeholder="Nhập tên thành phố tìm kiếm..." />
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+export default {
+  name: "AutocompleteView",
+  data() {
+    return {};
+  },
+  components: {
+    Navbar,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.auto-layout {
+  display: flex;
+  .input-layout {
+    margin: 10px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 400px;
+    height: 49px;
+    padding: 8px 10px;
+    gap: 4px;
+    background: rgba(230, 249, 255, 0.2);
+    border: 1px solid #dbdbdb;
+    border-radius: 4px;
+    flex: none;
+    order: 0;
+    align-self: stretch;
+    flex-grow: 0;
+  }
+  input {
+    width: 352px;
+    height: 20px;
+    font-family: "Noto Sans";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 20px;
+    color: #bfbfbf;
+    outline: none;
+    border: none;
+    display: flex;
+    align-items: center;
+    flex: none;
+    order: 1;
+    flex-grow: 1;
+  }
+}
+</style>
