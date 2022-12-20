936b9730aef3d39d6b748c4ee8f8ae3b790255bc
KeenTraan <tranmanhphong3103@gmail.com>
2022-12-20 22:50:12 +0700

    update ui Autocomplete

---
+++ src/components/Autocomplete/AutoList.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="list-item">
+    <div v-for="city in getCity" :key="city.code">
+      <ul>
+        <li class="item" @click="choiceItem">
+          {{ city.name }}
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+export default {
+  name: "AutoList",
+  methods: {
+    choiceItem() {
+      console.log("click me");
+    },
+  },
+  created() {
+    this.$store.dispatch("fetchCity");
+  },
+  computed: {
+    ...mapGetters(["getCity"]),
+  },
+};
+</script>
+
+<style>
+.list-item {
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  height: 128px;
+  align-items: flex-start;
+  padding: 0px;
+  border-radius: 4px;
+  cursor: grabbing;
+}
+.list-item::-webkit-scrollbar {
+  width: 5px;
+  height: 0;
+}
+.list-item::-webkit-scrollbar-thumb {
+  border-radius: 100rem;
+  background-color: grey;
+  height: 50px;
+}
+.list-item::-webkit-scrollbar-track {
+  border-radius: 100rem;
+  background-color: white;
+}
+.item {
+  cursor: pointer;
+  background-color: #f1f5f8;
+  padding: 10px;
+  width: 400px;
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 10px;
+}
+.item:hover {
+  background-color: #617D98;
+  transition: 1s;
+}
+</style>
--- src/views/AutoComplete.vue
+++ src/views/AutoComplete.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="auto-layout">
     <Navbar />
-    <div class="input-layout">
-      <img src="@/assets/search.png" alt="" />
-      <input type="search" placeholder="Nhập tên thành phố tìm kiếm..." />
+    <div>
+      <Search />
+      <AutoList />
     </div>
   </div>
 </template>
 
 <script>
 import Navbar from "@/components/Navbar.vue";
+import AutoList from "@/components/Autocomplete/AutoList.vue";
+import Search from "@/components/Autocomplete/Search.vue";
 export default {
   name: "AutocompleteView",
   data() {
@@ -17,6 +19,8 @@ export default {
   },
   components: {
     Navbar,
+    AutoList,
+    Search
   },
 };
 </script>
@@ -24,39 +28,5 @@ export default {
 <style lang="scss" scoped>
 .auto-layout {
   display: flex;
-  .input-layout {
-    margin: 10px;
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    width: 400px;
-    height: 49px;
-    padding: 8px 10px;
-    gap: 4px;
-    background: rgba(230, 249, 255, 0.2);
-    border: 1px solid #dbdbdb;
-    border-radius: 4px;
-    flex: none;
-    order: 0;
-    align-self: stretch;
-    flex-grow: 0;
-  }
-  input {
-    width: 352px;
-    height: 20px;
-    font-family: "Noto Sans";
-    font-style: normal;
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 20px;
-    color: #bfbfbf;
-    outline: none;
-    border: none;
-    display: flex;
-    align-items: center;
-    flex: none;
-    order: 1;
-    flex-grow: 1;
-  }
 }
 </style>
