2200f016bb42626d973008cd6d959d3f0177a2af
KeenTraan <tranmanhphong3103@gmail.com>
2022-12-27 23:37:30 +0700

    format code

--- src/views/CityView.vue
+++ src/views/CityView.vue
@@ -2,13 +2,13 @@
   <div class="city-layout">
     <Navbar />
     <AutoComplete
-    :options="cities"
-    :getSelect="selectedCities"
-    :placeholder="placeholder"
-    :filtersItem="filtersItem"
-    @searchOptions="searchOptions"
-    @addChosen="addChosen"
-    @deleteItem="deleteItem"
+      :options="cities"
+      :getSelect="selectedCities"
+      :placeholder="placeholder"
+      :filtersItem="filtersItem"
+      @searchOptions="searchOptions"
+      @addChosen="addChosen"
+      @deleteItem="deleteItem"
     />
   </div>
 </template>
@@ -16,49 +16,53 @@
 <script>
 import AutoComplete from "@/components/Autocomplete/AutoComplete";
 import Navbar from "@/components/Navbar.vue";
-import { mapActions, mapGetters } from 'vuex';
+import { mapActions, mapGetters } from "vuex";
 import { PLACEHOLDER } from "@/constant";
 export default {
-  data(){
+  data() {
     return {
       placeholder: PLACEHOLDER.CITY,
-      keyWord: ""
-    }      
+      keyWord: "",
+    };
   },
   components: {
     AutoComplete,
     Navbar,
   },
   methods: {
-    ...mapActions({ fetchCity: "fetchCity", addChosenCity: "selectCity", deleteCity: "deleteCity"}),
+    ...mapActions({
+      fetchCity: "fetchCity",
+      addChosenCity: "selectCity",
+      deleteCity: "deleteCity",
+    }),
     searchOptions(keyword) {
-      this.keyWord = keyword.trim()
+      this.keyWord = keyword.trim();
     },
     addChosen(item) {
-     this.addChosenCity(item)
-     this.keyWord = ""
+      this.addChosenCity(item);
+      this.keyWord = "";
     },
     deleteItem(item) {
-      this.deleteCity(item)
-    }
+      this.deleteCity(item);
+    },
   },
   computed: {
     ...mapGetters({
-      cities: "getCity", 
-      selectedCities: "getSelect"
+      cities: "getCity",
+      selectedCities: "getSelect",
     }),
     filtersItem() {
       return this.cities.filter((item) => {
         return (
-          item.name.toLowerCase().includes(this.keyWord.toLowerCase())
-          && this.keyWord.length
+          item.name.toLowerCase().includes(this.keyWord.toLowerCase()) &&
+          this.keyWord.length
         );
       });
     },
   },
   created() {
-   this.fetchCity();
-  }
+    this.fetchCity();
+  },
 };
 </script>
 
