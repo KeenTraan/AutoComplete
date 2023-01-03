566df40ba06cab59a0ba0933fae59f5ea99c14b5
KeenTraan <tranmanhphong3103@gmail.com>
2023-01-03 23:30:21 +0700

    convert file zie

---
+++ src/constant/Dopzone.js
@@ -0,0 +1,14 @@
+export const MAX_SIZE = 10240;
+export const TYPE_FILE = [
+  ".pdf",
+  ".doc",
+  ".docx",
+  ".pub",
+  ".xls",
+  ".xlsx",
+  ".txt",
+  ".png",
+  ".jpeg",
+  ".jpg",
+]
+export const NUMBER_BYTES = 1024
