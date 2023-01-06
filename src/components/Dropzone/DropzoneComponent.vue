901b6d880a24b1a9cb35014d04cc8c916a423c20
keenTraan <tranmanhphong3103@gmail.com>
2023-01-06 16:55:14 +0700

    upload file

--- src/components/Dropzone/DropzoneComponent.vue
+++ src/components/Dropzone/DropzoneComponent.vue
@@ -2,7 +2,7 @@
   <div>
     <div
       class="dropzone"
-      :class="{ 'error-vali': valid }"
+      :class="{ 'error-vali': valid, 'success-vali': success}"
       @dragover="dragFiles"
       @dragleave="dragLeave"
       @drop="dropFiles"
@@ -24,16 +24,18 @@
     <div class="error-message">
       {{ errorMessage }}
     </div>
-    <FileList 
-      :fileList="fileList"
-      @deleteFile="deleteFile" 
-     />
-     <button class="btn-upload" @click="uploadFile">Upload</button>
+    <div class="success-message">
+      {{ successMessage }}
+    </div>
+    <FileList :fileList="fileList" @deleteFile="deleteFile" />
+    <button v-if="!isHiden" class="btn-upload" @click="uploadFile">Upload</button>
   </div>
 </template>
 
 <script>
-import { ERROR_MESSAGE, LIMITED_FILE, MAX_SIZE} from "@/constant/Dopzone";
+import { MESSAGE, LIMITED_FILE, MAX_SIZE } from "@/constant/Dopzone";
+import { storage } from "@/configs/firebase";
+import { ref, uploadBytes } from "firebase/storage";
 import FileList from "@/components/Dropzone/FileListComponent.vue";
 export default {
   data() {
@@ -43,6 +45,7 @@ export default {
       isValid: true,
       isDragging: false,
       errorMessage: "",
+      successMessage: "",
     };
   },
   components: {
@@ -58,16 +61,17 @@ export default {
       newDataFile.forEach((item) => {
         if (item.size / 1024 > MAX_SIZE) {
           this.isValid = false;
-          this.errorMessage = ERROR_MESSAGE.SIZE_ERROR;
+          this.errorMessage = MESSAGE.SIZE_ERROR;
         }
       });
       if (newDataFile.length > this.limitedFile) {
         this.isValid = false;
-        this.errorMessage = ERROR_MESSAGE.LIMITED_ERROR;
+        this.errorMessage = MESSAGE.LIMITED_ERROR;
       }
       if (this.isValid) {
         this.fileList = newDataFile;
       }
+      this.$refs.file.value = [];
     },
     dragFiles(e) {
       e.preventDefault();
@@ -88,13 +92,31 @@ export default {
       });
     },
     uploadFile() {
-      console.log("let do dit");
-    }
+      let files = this.fileList;
+      for (let i = 0; i < files.length; i++) {
+        let file = files[i];
+        let storageRef = ref(storage, "Files/" + file.name);
+        uploadBytes(storageRef, file)
+          .then(() => {
+            this.successMessage = MESSAGE.SUCCESSFULLY
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+          this.fileList = [];
+      }
+    },
   },
   computed: {
     valid() {
       return this.errorMessage.length > 0;
     },
+    success() {
+      return this.successMessage.length > 0;
+    },
+    isHiden() {
+      return this.fileList.length === 0 
+    } 
   },
 };
 </script>
@@ -139,8 +161,19 @@ export default {
   font-size: 14px;
   line-height: 20px;
 }
+.success-message {
+  color: green;
+  margin-top: 17px;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+}
 .error-vali {
   border: 1px solid red;
+}
+.success-vali {
+  border: 1px solid green;
 }
 .btn-upload {
   margin-top: 10px;
--- src/constant/Dopzone.js
+++ src/constant/Dopzone.js
@@ -1,9 +1,10 @@
 export const MAX_SIZE = 10240;
 export const LIMITED_FILE = 3
-export const ERROR_MESSAGE = {
+export const MESSAGE = {
   SIZE_ERROR: "File size must be less than 10MB",
   LIMITED_ERROR: "File Limited 3",
-  DUPLICATE_ERROR: "file existed"
+  DUPLICATE_ERROR: "file existed",
+  SUCCESSFULLY: 'Upload file successfully'
 }
 export const TYPE_FILE = {
   DOCX: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
