d86e7623885ef2ae8dd61a62888222daed9b9728
keenTraan <tranmanhphong3103@gmail.com>
2023-01-10 13:31:04 +0700

    update code

--- src/utils/FormatFileSize.js
+++ src/utils/FormatFileSize.js
@@ -1,9 +1,7 @@
-const formatBytes = (bytes, decimals = 2) => {
-    if (!+bytes) return "0 bytes";
-    const k = 1024;
-    const dm = decimals < 0 ? 0 : decimals
-    const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB"];
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
-    return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
+const formatBytes = (value) => {
+    let  sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (value === 0) return '0 Byte';
+    let i = parseInt(Math.floor(Math.log(value) / Math.log(1024)));
+    return Math.round(value / Math.pow(1024, i), 2) + ' ' + sizes[i];
 }
 export default formatBytes;
