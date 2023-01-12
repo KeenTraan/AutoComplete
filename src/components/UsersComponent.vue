92820b2021e6b86eac6a4bb587f4a939618be6f2
keenTraan <tranmanhphong3103@gmail.com>
2023-01-12 10:34:23 +0700

    update code

---
+++ src/components/UsersComponent.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="users-layout">
+    <div class="body-layout">
+      <AutoComplete
+        :options="users"
+        :placeholder="placeholder"
+        :filtersItem="filtersItem"
+        :getSelect="selectUsers"
+        @searchOptions="searchOptions"
+        @addChosen="addChosen"
+        @deleteItem="deleteItem"
+      />
+      <DropzoneComponent :limitedFile="3" :maxSize="10000000"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import AutoComplete from "@/components/Autocomplete/AutoComplete";
+import { mapActions, mapGetters } from "vuex";
+import { PLACEHOLDER } from "@/constant/Autocomplete";
+import DropzoneComponent from '@/components/Dropzone/DropzoneComponent.vue';
+export default {
+  name: "UserView",
+  components: {
+    AutoComplete,
+    DropzoneComponent,
+  },
+  data() {
+    return {
+      placeholder: PLACEHOLDER.USERS,
+      keyWord: "",
+    };
+  },
+  methods: {
+    ...mapActions({
+      fetchUsers: "fetchUsers",
+      addChosenUser: "selectUsers",
+      deleteUsers: "deleteUsers",
+    }),
+
+    searchOptions(keyword) {
+      this.keyWord = keyword;
+    },
+    addChosen(item) {
+      this.addChosenUser(item);
+    },
+    deleteItem(item) {
+      this.deleteUsers(item);
+    },
+  },
+  computed: {
+    ...mapGetters({
+      users: "getUsers",
+      selectUsers: "getSelectUser",
+    }),
+    filtersItem() {
+      return this.users.filter((item) => {
+        return (
+          this.keyWord.length &&
+          item.name.toLowerCase().includes(this.keyWord.toLowerCase())
+        );
+      });
+    },
+  },
+  created() {
+    this.fetchUsers();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.users-layout {
+  display: flex;
+  .body-layout {
+    display: flex;
+    flex-direction: column;
+  }
+}
+</style>
--- src/views/UsersView.vue
+++ src/views/UsersView.vue
@@ -1,84 +1,23 @@
 <template>
-  <div class="users-layout">
+  <div class="user-view">
     <Navbar />
-    <div class="body-layout">
-      <AutoComplete
-        :options="users"
-        :placeholder="placeholder"
-        :filtersItem="filtersItem"
-        :getSelect="selectUsers"
-        @searchOptions="searchOptions"
-        @addChosen="addChosen"
-        @deleteItem="deleteItem"
-      />
-      <DropzoneComponent :limitedFile="3" :maxSize="10000000"/>
-    </div>
+    <UsersComponent />
   </div>
 </template>
 
 <script>
-import AutoComplete from "@/components/Autocomplete/AutoComplete";
-
 import Navbar from "@/components/Navbar.vue";
-import { mapActions, mapGetters } from "vuex";
-import { PLACEHOLDER } from "@/constant/Autocomplete";
-import DropzoneComponent from '@/components/Dropzone/DropzoneComponent.vue';
+import UsersComponent from "@/components/UsersComponent.vue";
 export default {
-  name: "UserView",
   components: {
-    AutoComplete,
+    UsersComponent,
     Navbar,
-    DropzoneComponent,
-  },
-  data() {
-    return {
-      placeholder: PLACEHOLDER.USERS,
-      keyWord: "",
-    };
-  },
-  methods: {
-    ...mapActions({
-      fetchUsers: "fetchUsers",
-      addChosenUser: "selectUsers",
-      deleteUsers: "deleteUsers",
-    }),
-
-    searchOptions(keyword) {
-      this.keyWord = keyword;
-    },
-    addChosen(item) {
-      this.addChosenUser(item);
-    },
-    deleteItem(item) {
-      this.deleteUsers(item);
-    },
-  },
-  computed: {
-    ...mapGetters({
-      users: "getUsers",
-      selectUsers: "getSelectUser",
-    }),
-    filtersItem() {
-      return this.users.filter((item) => {
-        return (
-          this.keyWord.length &&
-          item.name.toLowerCase().includes(this.keyWord.toLowerCase())
-        );
-      });
-    },
-  },
-  created() {
-    this.fetchUsers();
   },
 };
 </script>
 
-<style lang="scss" scoped>
-.users-layout {
+<style scoped lang="scss">
+.user-view {
   display: flex;
-  .body-layout {
-    display: flex;
-    flex-direction: column;
-  }
 }
 </style>
